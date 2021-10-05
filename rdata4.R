db8737cb60fa7eb81b00f90bb6e61c4308f40b28
shahab sajid <91873176+ShahabSajid@users.noreply.github.com>
2021-10-05 12:50:18 +0500

    Add files via upload

---
+++ rdata4.R
@@ -0,0 +1,20 @@
+n = c(2,3,4,5)
+s = c('aa','bb','cc','dd')
+b = c(TRUE,FALSE,TRUE,FALSE)
+df = data.frame(n,s,b)
+df
+mtcars
+mtcars[1,2]
+mtcars['Duster 360','disp']
+ncol(mtcars)
+head(mtcars)
+mtcars[[9]]
+mtcars[['am']]
+mtcars$drat
+mtcars[,'hp']
+mtcars[1]
+mtcars['wt']
+mtcars[c('hp','gear')]
+mtcars[25,]
+mtcars[c(3,28),]
+mtcars['Lotus Europa',]
